c8dd25c80a4ea75e3cebc04f4f884a61ea36b912
Alejo <alejo.gutierrez.adeje@gmail.com>
2026-05-12 17:59:55 +0100

    feat: añadidas entidades User, RestaurantTable y Reservation

---
+++ restoreserve/src/main/java/com/gastrotech/restoreserve/entity/User.java
@@ -0,0 +1,27 @@
+package com.gastrotech.restoreserve.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
