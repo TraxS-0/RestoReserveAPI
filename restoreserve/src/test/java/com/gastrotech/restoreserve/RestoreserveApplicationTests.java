920468300a0982cc7d72c28dfe4a3088b38c7265
Alejo <alejo.gutierrez.adeje@gmail.com>
2026-05-12 17:50:06 +0100

    chore: estructura inicial de carpetas y configuración de proyecto

---
+++ restoreserve/src/test/java/com/gastrotech/restoreserve/RestoreserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastrotech.restoreserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoreserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
